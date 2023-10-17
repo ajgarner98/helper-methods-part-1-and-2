8a1c8656ab2c2eabdc0d2589526b5d9695a3ae06
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-10-17 21:53:49 +0000

    Fixed form for new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -30,8 +30,9 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie_attributes = params.fetch(:movie)
+    movie_attributes = params.require(:movie).permit(:title, :description)
     @movie = Movie.new(movie_attributes)
+    
     #@movie = Movie.new
     #@movie.title = params.fetch(:movie).fetch(:title)
     #@movie.description = params.fetch(:movie).fetch(:description)
